0ab422a13217082b940ce67a89c4ab9a5d9311f3
adair <adairaxe31@hotmail.com>
2026-01-11 19:20:30 -0500

    ready one version

--- src/lib/services/api.ts
+++ src/lib/services/api.ts
@@ -1,20 +1,18 @@
 export async function fetchWorks() {
-    const response = await fetch(`${import.meta.env.VITE_BACKEND_FRASES}/works`);
+    const response = await fetch(`${import.meta.env.VITE_BACKEND_FRASES}/api/works`);
     if (!response.ok) {
         throw new Error('Failed to fetch works');
     }
     return await response.json();
 }
 
-
 export async function fetchWorkById(idWork: string) {
-    const response = await fetch(`${import.meta.env.VITE_BACKEND_FRASES}/works/${idWork}`);
+    const response = await fetch(`${import.meta.env.VITE_BACKEND_FRASES}/api/works/${idWork}`);
     if (!response.ok) {
         throw new Error('Failed to fetch work');
     }
     return await response.json();
 }
-
 
 export async function saveWork({ type, title, director, year }: {
     type: string;
@@ -22,38 +20,32 @@ export async function saveWork({ type, title, director, year }: {
     director: string;
     year: string
 }) {
-
-    const response = await fetch(`${import.meta.env.VITE_BACKEND_FRASES}/works/`, {
+    const response = await fetch(`${import.meta.env.VITE_BACKEND_FRASES}/api/works/`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({ type, title, director, year })
     });
-
     if (!response.ok) {
         throw new Error('Failed to save work');
     }
-
     return await response.json();
 }
-
 
 export async function saveQuote({ quote, idWork }: {
     quote: string;
     idWork: string;
 }) {
-    const response = await fetch(`${import.meta.env.VITE_BACKEND_FRASES}/quotes/`, {
+    const response = await fetch(`${import.meta.env.VITE_BACKEND_FRASES}/api/quotes/`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({ 'text': quote, 'work': idWork })
     });
-
     if (!response.ok) {
         throw new Error('Failed to save quote');
     }
-
     return await response.json();
 }
